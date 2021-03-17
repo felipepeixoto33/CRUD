c83f86eb4e59d6922532aad20fb61956fb280234
Felipe Peixoto <felipepeixoto33@hotmail.com>
2021-03-17 17:21:56 -0300

    0.0.0.6
    Integration with FIrebase is basically finished.

--- src/app/pages/employees/employees.service.ts
+++ src/app/pages/employees/employees.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Employee } from 'src/app/shared/models/employee.interface';
 import { AngularFirestoreCollection } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +12,40 @@ export class EmployeesService {
 
   private employeesCollection: AngularFirestoreCollection<Employee>;
 
-  constructor(private readonly afs: AngularFirestore) {}
+  constructor(private readonly afs: AngularFirestore) {
+    this.employeesCollection = afs.collection<Employee>('employees');
+    this.getEmployees();
+  }
+
+  onDeleteEmployee(empId: string): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const result = this.employeesCollection.doc(empId).delete();
+        resolve(result);
+      } catch (err) {
+        reject(err.message);
+      }
+    });
+  }
+
+  onSaveEmployee(employee: Employee, empId: string): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const id = empId || this.afs.createId();
+        const data = { id, ...employee };
+        const result = this.employeesCollection.doc(id).set(data);
+        resolve(result);
+      } catch (err) {
+        reject(err.message);
+      }
+    });
+  }
+
+  private getEmployees(empId: string): void {
+    this.employees = this.employeesCollection
+      .snapshotChanges()
+      .pipe(
+        map((actions) => actions.map((a) => a.payload.doc.data() as Employee))
+      );
+  }
 }
